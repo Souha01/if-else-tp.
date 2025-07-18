ca3b86d0ffc27d0d89275d6d7af8e2538a92cd07
souha <athmanisouheila@gmail.com>
2025-07-18 14:01:01 +0200

    if_else_tp

---
+++ _if_else_[athmani souheila].py
@@ -0,0 +1,17 @@
+firstname=input("Enter your first name: ")
+lastname=input("Enter your last name")
+
+if firstname and lastname:
+    print(f"Welcome, {firstname} {lastname}")
+else:
+    print("Error: Both first name and last name must be provided.")
+
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+if username == "admin" and password == "password123":
+    print("Login successful.")
+elif username != "admin":
+    print("Error: Invalid username.")
+else:
+    print("Error: Invalid password.")
